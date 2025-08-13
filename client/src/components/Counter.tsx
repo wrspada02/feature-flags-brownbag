4cd0fbd3a2438e6a26f61e85900869e21ed6f9f7
William Spada <williamrspada02@gmail.com>
2025-08-13 11:52:29 -0300

    feat: add counter component

---
+++ client/src/components/Counter.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+export function Counter() {
+  const [counter, setCounter] = useState<number>(0);
+
+  return (
+    <section className="bg-[#FAFAFA] p-5 rounded-lg shadow-md text-black">
+      <h1 className="font-bold">Feature Flag - Counter</h1>
+      <p>This feature flag controls the visibility of the counter component.</p>
+      <button onClick={() => setCounter(counter + 1)} className="bg-blue-500 text-white p-2 rounded cursor-pointer">
+        Current Value: <span className="font-bold">{counter}</span>
+      </button>
+    </section>
+  )
+}
