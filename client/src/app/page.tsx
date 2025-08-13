7f3ba23ccb9d1bbabb9f1ab994116633e972b1a0
William Spada <williamrspada02@gmail.com>
2025-08-13 17:39:20 -0300

    chore: approach with app router to request data

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,6 +1,10 @@
 import { Counter } from "@/components/Counter";
 
-export default function Home() {
+export default async function Home() {
+  const featureFlags = await fetch("http://localhost:3000/api/v1/feature_flags");
+  const flags = await featureFlags.json();
+  console.log({ flags });
+
   return (
     <Counter />
   );
