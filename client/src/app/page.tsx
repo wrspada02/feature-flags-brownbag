4b8d17838db319a00ca894b5fe4b095789e7fc40
William Spada <williamrspada02@gmail.com>
2025-08-13 17:24:45 -0300

    chore: adjust layout

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,10 +1,7 @@
-'use client'
 import { Counter } from "@/components/Counter";
 
 export default function Home() {
   return (
-    <main className="w-screen h-screen bg-[#292323] p-10">
-      <Counter />
-    </main>
+    <Counter />
   );
 }
